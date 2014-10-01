dd0e6aea6ca8a8052a078ea808692bc058b6e06c
inigovinas <inigovinas@hotmail.com>
2014-10-01 11:55:16 +0200

    v1

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>forms</title>
+</head>
+<body>
+
+	<h1>Aupa!!</h1>
+
+    <form action="hello.php" method="get">
+        Name: <input type="text" name="name"><br>
+        <input type="submit">
+    </form>
+
+
+
+</body>
+</html>
